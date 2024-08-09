e106a633097201f10b2db5a4446e1b2d219ad60c
W. Leighton Dawson <leighton@thinkst.com>
2024-08-09 09:29:20 +0200

    Make page installable as a PWA (#4)
    * Add service worker; allow local image sources
    
    * Add https: and static/* to img-src CSP header
    
    * Add Content-Type headers
    
    * Service-Worker-Allowed: /

---
+++ src/static/sw.js
@@ -0,0 +1,16 @@
+"use strict"
+self.addEventListener('install', event => {
+  event.waitUntil((async () => {
+  })());
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith((async () => {
+    try {
+      const fetchResponse = await fetch(event.request);
+      return fetchResponse;
+    } catch (e) {
+      // The network failed
+    }
+  })());
+}); 
